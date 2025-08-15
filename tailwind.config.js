713c9c642068798ccee7231e76f5ecacc6ad1087
laurentlaboise <110203851+laurentlaboise@users.noreply.github.com>
2025-08-15 13:44:10 +0000

    TAILWIND

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.html',
+    './en/**/*.html',
+    './lo/**/*.html',
+    './th/**/*.html',
+    './fr/**/*.html',
+    './js/**/*.js',
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
