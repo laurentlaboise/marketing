2e6f2c0be295b20a3d016e120dab34d21259ff7f
laurentlaboise <110203851+laurentlaboise@users.noreply.github.com>
2025-09-06 22:41:51 +0700

    Delete js/modules/supabase.js

--- js/modules/supabase.js
+++
@@ -1,8 +0,0 @@
-// js/modules/supabase.js
-
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-
-const supabaseUrl = 'https://msivaavxwszurzopourl.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zaXZhYXZ4d3N6dXJ6b3BvdXJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMyNTAzMDAsImV4cCI6MjA2ODgyNjMwMH0.BznUDkfio5o83f7ZsYyTgrN-oa8NkPy5I1Wqiq46x78';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
